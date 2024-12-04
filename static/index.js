df5940175d789c162c8915a31732b9e65abf19a7
olegjb <oleg.jboitsov@gmail.com>
2024-12-04 15:22:03 -0700

    test ability to call-in nav-bar

--- static/index.js
+++ static/index.js
@@ -1,3 +1,11 @@
+import { navBar } from "./nav-menu.js";
+
 console.log('index script!')
 
+
+const bod = document.querySelector('body')
+
+if (bod) {
+    bod.appendChild(navBar())
+}
 
--- static/nav-menu.js
+++ static/nav-menu.js
@@ -1,8 +1,35 @@
 console.log('nav bar/menu script')
 
 
-// fetch the template
+function navBar() {
+    'returns a nav-bar component'
+    const newDiv = document.createElement('div')
+    newDiv.classList.add('nav-container')
+
+    // fetch the template
+    fetch ('/nav-bar')
+        .then( content => {
+            // read as text 
+            return content.text()
+        })
+
+        
+        // return it as 'html'
+        .then ( teContent => {
+            
+            newDiv.innerHTML = teContent
+        })
+
+    return newDiv
+
+}
 
 
-// render as html
+export {
+    navBar
+}
+
+
+
+
 
